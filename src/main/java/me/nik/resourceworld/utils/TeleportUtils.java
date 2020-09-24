f1175921e4e7b9ad4c754bfb69df2b3ab314a145
Nik <61439310+NikV2@users.noreply.github.com>
2020-09-24 17:54:38 +0300

    Minor improvements

--- src/main/java/me/nik/resourceworld/utils/TeleportUtils.java
+++ src/main/java/me/nik/resourceworld/utils/TeleportUtils.java
@@ -29,42 +29,47 @@ public class TeleportUtils {
         switch (environment) {
             case NETHER:
                 x = random.nextInt(Config.Setting.TELEPORT_NETHER_MAX_RANGE.getInt());
+                y = 80;
                 z = random.nextInt(Config.Setting.TELEPORT_NETHER_MAX_RANGE.getInt());
-                break;
+                boolean safe = false;
+                while (!safe) {
+                    randomLocation = new Location(world, x, y, z);
+                    if (!randomLocation.getBlock().isEmpty()) {
+                        randomLocation.setY(y + 1);
+                        safe = true;
+                    } else y--;
+                }
+                while (!isLocationSafe(randomLocation)) {
+                    randomLocation = generateLocation(world);
+                }
+                return randomLocation;
             case THE_END:
                 x = random.nextInt(Config.Setting.TELEPORT_END_MAX_RANGE.getInt());
                 z = random.nextInt(Config.Setting.TELEPORT_END_MAX_RANGE.getInt());
-                break;
+                randomLocation = new Location(world, x, y, z);
+                y = randomLocation.getWorld().getHighestBlockYAt(randomLocation) + 1;
+                randomLocation.setY(y);
+                Material below = randomLocation.getWorld().getBlockAt(x, y - 3, z).getType();
+                if (!below.isSolid()) {
+                    randomLocation = generateLocation(world);
+                }
+                return randomLocation;
             default:
                 x = random.nextInt(Config.Setting.TELEPORT_WORLD_MAX_RANGE.getInt());
                 z = random.nextInt(Config.Setting.TELEPORT_WORLD_MAX_RANGE.getInt());
-                break;
+                boolean safee = false;
+                while (!safee) {
+                    randomLocation = new Location(world, x, y, z);
+                    if (!randomLocation.getBlock().isEmpty()) {
+                        randomLocation.setY(y + 1);
+                        safee = true;
+                    } else y--;
+                }
+                while (!isLocationSafe(randomLocation)) {
+                    randomLocation = generateLocation(world);
+                }
+                return randomLocation;
         }
-
-        if (environment == World.Environment.THE_END) {
-            randomLocation = new Location(world, x, y, z);
-            y = randomLocation.getWorld().getHighestBlockYAt(randomLocation) + 1;
-            randomLocation.setY(y);
-            Material below = randomLocation.getWorld().getBlockAt(x, y - 3, z).getType();
-            if (!below.isSolid()) {
-                randomLocation = generateLocation(world);
-            }
-            return randomLocation;
-        } else {
-            boolean safe = false;
-            while (!safe) {
-                randomLocation = new Location(world, x, y, z);
-                if (!randomLocation.getBlock().isEmpty()) {
-                    randomLocation.setY(y + 1);
-                    safe = true;
-                } else y--;
-            }
-        }
-
-        while (!isLocationSafe(randomLocation)) {
-            randomLocation = generateLocation(world);
-        }
-        return randomLocation;
     }
 
     private boolean isLocationSafe(Location location) {
