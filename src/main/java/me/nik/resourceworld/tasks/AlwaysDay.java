d2c7605c104c4ddc9f954bc9608ac73f47e1e0b3
Nik <61439310+NikV2@users.noreply.github.com>
2024-11-24 01:05:12 +0200

    Update

--- src/main/java/me/nik/resourceworld/tasks/AlwaysDay.java
+++ src/main/java/me/nik/resourceworld/tasks/AlwaysDay.java
@@ -8,6 +8,6 @@ public class AlwaysDay extends BukkitRunnable {
 
     @Override
     public void run() {
-        Bukkit.getWorld(Config.Setting.WORLD_NAME.getString()).setTime(1000);
+        Bukkit.getWorld(Config.Setting.WORLD_NAME.getString()).setTime(1000L);
     }
 }
